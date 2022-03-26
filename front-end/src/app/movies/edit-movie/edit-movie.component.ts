69ede8dae4fa985820bad28a1dc23c21eadb5355
Arunas <victornotsurrender@gmail.com>
2022-03-26 15:31:36 -0500

    Primeras modificaciones en algunos archivos

---
+++ front-end/src/app/movies/edit-movie/edit-movie.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-movie',
+  templateUrl: './edit-movie.component.html',
+  styleUrls: ['./edit-movie.component.css']
+})
+export class EditMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front-end/src/app/theaters/edit-theater/edit-theater.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-theater',
+  templateUrl: './edit-theater.component.html',
+  styleUrls: ['./edit-theater.component.css']
+})
+export class EditTheaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
